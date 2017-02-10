4621a50a4a838346fb162880b3f899271fafd8e7
bord81 <test1@MacBook-Pro-Test1.local>
2017-02-10 12:25:58 +0200

    Some comments added

--- LinkedList1.java
+++ LinkedList1.java
@@ -1,26 +1,26 @@
-
-package Emul;
-
-
-public class LinkedList1 <E> {
-    private static class Node <E> {
+/*
+Simple implementation of two-way linked list.
+ */
+public class LinkedList1<E> {
+//helper class to create a Node instance
+    private static class Node<E> {
 
         private E data;
         private Node prev;
         private Node next;
 
-        public Node(E data, Node prev, Node next) {
+        public Node(E data, Node prev, Node next) { 
             this.data = data;
             this.prev = prev;
             this.next = next;
         }
     }
 
-    private Node head;
+    private Node head;  //pointers to nodes
     private Node last;
     private int elements = 0;
-
-    public void add(E data) {
+//standard add method
+    public void add(E data) { 
         if (head == null) {
             head = new Node(data, null, null);
             last = head;
@@ -32,8 +32,8 @@ public class LinkedList1 <E> {
             elements++;
         }
     }
-
-    public void addInv(E data) {
+//'ínverted' add method
+    public void addInv(E data) { 
         if (head == null) {
             last = new Node(data, null, null);
             head = last;
@@ -45,8 +45,8 @@ public class LinkedList1 <E> {
             elements++;
         }
     }
-
-    public void add(int index, E elem) {
+//add by index
+    public void add(int index, E elem) { 
         if (elements == 0 || index < 0 || index >= elements) {
             outOfBounds();
         }
@@ -83,8 +83,8 @@ public class LinkedList1 <E> {
 
         }
     }
-
-    public void set(int index, E elem) {
+//set the value of an element
+    public void set(int index, E elem) { 
         if (elements == 0 || index < 0 || index >= elements) {
             outOfBounds();
         }
@@ -103,7 +103,7 @@ public class LinkedList1 <E> {
         }
 
     }
-
+//get the value of an element
     public E get(int index) {
         if (elements == 0 || index < 0 || index >= elements) {
             outOfBounds();
@@ -125,8 +125,8 @@ public class LinkedList1 <E> {
 
         return elem;
     }
-
-    public void remove(int index) { 
+//remove node
+    public void remove(int index) {
         if (elements == 0 || index < 0 || index >= elements) {
             outOfBounds();
         }
@@ -167,13 +167,13 @@ public class LinkedList1 <E> {
 
         }
     }
-
+//delete all nodes
     public void clear() {
         head = null;
         last = head;
         elements = 0;
     }
-
+//check if list contains a specific element
     public boolean contains(E elem) {
         if (isEmpty()) {
             return false;
@@ -188,7 +188,7 @@ public class LinkedList1 <E> {
         }
         return false;
     }
-
+//find the first index of a specific element
     public int indexOf(E elem) {
         if (isEmpty()) {
             return -1;
@@ -203,8 +203,8 @@ public class LinkedList1 <E> {
         }
         return -1;
     }
-
-    public int lastIndexOf(E elem) { 
+//find the first index of a specific element
+    public int lastIndexOf(E elem) {
         if (isEmpty()) {
             return -1;
         }
@@ -219,24 +219,24 @@ public class LinkedList1 <E> {
         }
         return t;
     }
-
+//determine the siz of the list
     public int size() {
         int size = elements;
         return size;
     }
-
+//check if the list is empty
     public boolean isEmpty() {
         if (head != null) {
             return false;
         }
         return true;
     }
-
+//carve out a block from the list
     public LinkedList1 subList(int beginIndex, int postEndIndex) {
         if (elements == 0 || beginIndex < 0 || postEndIndex >= elements) {
             outOfBounds();
         }
-       LinkedList1 ll1 = new LinkedList1();
+        LinkedList1 ll1 = new LinkedList1();
         if (beginIndex < elements / 2 && (postEndIndex - elements / 2) <= elements / 2 - beginIndex) {
             Node temp = head;
             for (int i = 0; i < beginIndex; i++) {
@@ -260,7 +260,7 @@ public class LinkedList1 <E> {
         }
         return ll1;
     }
-
+//exception handling
     private void outOfBounds() {
         throw new IndexOutOfBoundsException("Specified index does not exist");
     }

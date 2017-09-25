bb3c62bb11ea2c77de3325a58e80a872e258bdff
bord81 <bslabut@gmail.com>
2017-09-25 22:05:46 +0300

    Create Euclid_GCD.java

---
+++ Euclid_GCD.java
@@ -0,0 +1,44 @@
+//Basic method for finding GCD (greatest common divisor) using Euclidean algorithm
+public class Euclid_GCD {
+
+    public static void main(String[] args) {
+        int a = 107002;
+        int b = 462;
+        System.out.println(euclidBase(a, b));
+    }
+
+    public static int euclidBase(int a, int b) {
+        int cycles = 0;
+        if (a == 0) {
+            return b;
+        } else if (b == 0) {
+            return a;
+        }
+        if (a == b) {
+            return a;
+        }
+        int swap_a;
+        int swap_b;
+        int div;
+        int rem;
+        if (a > b) {
+            swap_a = a;
+            swap_b = b;
+        } else {
+            swap_a = b;
+            swap_b = a;
+        }
+        while (true) {
+            cycles++;
+            div = swap_a / swap_b;
+            rem = swap_a % swap_b;
+            if (rem == 0) {
+                break;
+            } else {
+                swap_a = swap_b;
+                swap_b = rem;
+            }
+        }
+        return swap_b;
+    }
+}

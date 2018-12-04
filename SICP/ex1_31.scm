d733529959dd645bba76a9a5e8d24bb9132efb7d
bord81 <bslabut@gmail.com>
2018-12-04 09:32:43 +0200

    Update ex1_31.scm

--- SICP/ex1_31.scm
+++ SICP/ex1_31.scm
@@ -4,8 +4,9 @@
    (/ (* 4 (* n n)) (- (* 4 (* n n)) 1)))
 
 ;improved Wallis product ('empirical 3/4')
-;max computable n on my interpreter is 48, which gives 3.141551347008624
-;'the classical' version needs n = 20 000 to achieve comparable precision 
+;max computable n on my interpreter is 48 (float argument), which gives 3.141551347008624
+;'the classical' version needs n = 20 000 to achieve comparable precision
+;3.14159 is already achived by n = 192, i.e. (* (wallis-pi-impr 192) 1.0)
 (define (wallis-pi-impr n) 
    (/ (* (fast-expt 2 (+ (* 4 n) 2)) (+ n (/ 3 4)) (fast-expt (prod factorial 1 inc n) 4))
    (square (prod factorial 1 inc (+ (* 2 n) 1)))))

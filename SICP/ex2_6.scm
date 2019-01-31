96b9749e209c611c10c38c21f6b9330ba2c31c9d
bord81 <bslabut@gmail.com>
2019-01-31 07:55:24 +0200

    Create ex2_6.scm

---
+++ SICP/ex2_6.scm
@@ -0,0 +1,7 @@
+(define two (lambda (f) (lambda (x) (f (f x)))))
+
+(define one (lambda (f) (lambda (x) (f x))))
+
+(define zero (lambda (f) (lambda (x) x)))
+(define (add-1 n)
+  (lambda (f) (lambda (x) (f ((n f) x)))))
